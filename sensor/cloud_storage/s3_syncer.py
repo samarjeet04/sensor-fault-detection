6ad196a1e16244d9d0b62e14eff4993277a446b6
samarjeet04 <99862615+samarjeet04@users.noreply.github.com>
2023-01-15 11:10:52 +0530

    Updated

--- sensor/cloud_storage/s3_syncer.py
+++ sensor/cloud_storage/s3_syncer.py
@@ -6,6 +6,7 @@ class S3Sync:
         command = f"aws s3 sync {folder} {aws_buket_url} "
         os.system(command)
 
+        
     def sync_folder_to_s3(self,folder, aws_buket_url):
         command = f"aws s3 sync {folder} {aws_buket_url} "
         os.system(command)
